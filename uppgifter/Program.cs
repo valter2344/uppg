627f6b71d9f502fca9c9ddd4038539f62d73bc72
valter2344 <valter.lonnqvist65@gmail.com>
2024-11-20 13:00:46 +0200

    Add files via upload

---
+++ uppgifter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberAsString = "887";
+            int number = int.Parse(numberAsString);
+            Console.WriteLine("The value might be " + number);
+            Console.ReadLine();
+        }
+    }
+}
