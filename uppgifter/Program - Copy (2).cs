b4ac5c183ae34e1ccd746118893ec3233c3a8773
valter2344 <valter.lonnqvist65@gmail.com>
2024-11-21 12:24:05 +0200

    Add files via upload

---
+++ uppgifter/Program - Copy (2).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppppppppppppppppppppppppppppppg3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("o well hello " + name);
+
+        }
+    }
+}
