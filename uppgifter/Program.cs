95911a3005f1a554ab8786b2453d4a541b1e794f
valter2344 <valter.lonnqvist65@gmail.com>
2024-11-21 12:34:02 +0200

    Add files via upload

--- uppgifter/Program.cs
+++ uppgifter/Program.cs
@@ -1,20 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp12
+namespace uppg8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string numberAsString = "887";
-            int number = int.Parse(numberAsString);
-            Console.WriteLine("The value might be " + number);
-            Console.ReadLine();
+
+            {
+
+                {
+                    Console.Write("Please enter your age: ");
+                    string ageInput = Console.ReadLine();
+                    int Age = int.Parse(ageInput);
+                    const int LegalAge = 18;
+
+                    if (Age >= LegalAge)
+                    {
+                        Console.WriteLine("You are an adult.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are under the legal age.");
+                    }
+
+                    int Counter = 1;
+
+                    while (Counter <= 5)
+                    {
+                        Console.WriteLine(Counter);
+                        Counter++;
+                    }
+
+                    switch (Counter)
+                    {
+                        case 1:
+                            Console.WriteLine("One");
+                            break;
+                        case 2:
+                            Console.WriteLine("Two");
+                            break;
+                        case 3:
+                            Console.WriteLine("Three");
+                            break;
+                        case 4:
+                            Console.WriteLine("Four");
+                            break;
+                        case 5:
+                            Console.WriteLine("Five");
+                            break;
+                        default:
+                            Console.WriteLine("Enter a number between 18-99");
+                            break;
+                    }
+
+                    string name = "John";
+                    Console.WriteLine($"Welcome, {name}! The loop has completed, and the final value of Counter was {Counter - 1}.");
+
+
+                }
+            }
         }
     }
 }
